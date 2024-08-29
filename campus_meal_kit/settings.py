03aa51677efcc7a14232bcd9b3c09c733db3ae2e
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-08-29 23:27:45 +1000

    feat: add database

--- campus_meal_kit/settings.py
+++ campus_meal_kit/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +83,12 @@ WSGI_APPLICATION = 'campus_meal_kit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
--- campus_meal_kit/views.py
+++ campus_meal_kit/views.py
@@ -3,9 +3,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from applibs.response import prepare_success_response, prepare_error_response
 
+from .models import Item
+from .serializers import ItemSerializer
+
 
 class HomeView(APIView):
     def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+
         return Response(
-            prepare_success_response("Hello World!"), status=status.HTTP_200_OK
+            prepare_success_response(serializer.data), status=status.HTTP_200_OK
         )
