175bc63b485f164cf01a04e71590227d3471ae43
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-08-30 17:25:29 +1000

    fix: static url, middleware

--- campus_meal_kit/settings.py
+++ campus_meal_kit/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'campus_meal_kit',
     'rest_framework',
+    'corsheaders',
+    'gunicorn',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
