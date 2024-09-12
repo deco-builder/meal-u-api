388820b53219da7579eadd246281d08c4e79c564
Matthew-Rizky-Hartadi <matthew.rizky11@ui.ac.id>
2024-09-13 03:49:10 +1000

    chore: adjust to return order status name

--- orders/serializers/orders.py
+++ orders/serializers/orders.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from ..models import Orders
 
 class OrderSerializer(serializers.ModelSerializer):
+
+    order_status = serializers.SerializerMethodField()
+    
     class Meta:
         model = Orders
         fields = '__all__'  
+
+    def get_order_status(self, obj):
+        return obj.order_status.name 
