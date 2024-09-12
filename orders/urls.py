00c7682ab2f2381994501693f6c6dbe9ce5826ce
Matthew-Rizky-Hartadi <matthew.rizky11@ui.ac.id>
2024-09-13 06:38:46 +1000

    feat: add post delivery details endpoint

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views.orders import OrderListView, OrderStatusUpdateView
+from .views.delivery_details import AddDeliveryDetailsView
 
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/<int:order_id>/status/paid/', OrderStatusUpdateView.as_view(), name='order-status-paid'),
+    path('add-delivery-details/', AddDeliveryDetailsView.as_view(), name='add-delivery-details'),
+
 ]
