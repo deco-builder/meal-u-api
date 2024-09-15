7c11b9e6f7ea6b3e6d297404f82dc3dd44b9aebb
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-09-15 11:50:07 +1000

    feat: add image in category

--- groceries/models.py
+++ groceries/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
+    image = models.ImageField(upload_to="category/", blank=True, null=True)
 
     def __str__(self):
         return self.name
