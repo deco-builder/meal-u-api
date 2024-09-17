f54d9101cdac3f59d59ae7894a2c7831afe9c9c5
Matthew-Rizky-Hartadi <matthew.rizky11@ui.ac.id>
2024-09-17 22:46:40 +1000

    fix: remove unused path

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views.orders import OrderListView, OrderStatusUpdateView, OrderDetailView
-from .views.delivery_details import AddDeliveryDetailsView
 from .views.checkout import CheckoutView 
 from .views.delivery_time_slot import DeliveryTimeSlotListView
 from .views.delivery_location import DeliveryLocationListView
@@ -8,7 +7,6 @@ from .views.delivery_location import DeliveryLocationListView
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/<int:order_id>/status/paid/', OrderStatusUpdateView.as_view(), name='order-status-paid'),
-    path('add-delivery-details/', AddDeliveryDetailsView.as_view(), name='add-delivery-details'),
     path('orders/<int:order_id>/', OrderDetailView.as_view(), name='order-detail'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('delivery-time-slots/', DeliveryTimeSlotListView.as_view(), name='delivery-time-slots'),
