e55b846a18b96c0f6cabce672ee1c60262636cad
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-09-25 21:02:14 +1000

    feat: add get all orders filter warehouse

--- orders/views/orders_warehouse.py
+++ orders/views/orders_warehouse.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from user_auth.permission import IsWarehouseUser
 from applibs.response import prepare_success_response, prepare_error_response
 from ..services.orders_warehouse import OrderWarehouseService
+from datetime import datetime
 
 
 class OrderWarehouseView(APIView):
@@ -17,7 +18,22 @@ class OrderWarehouseView(APIView):
 
     def get(self, request):
         try:
-            response = self.order_warehouse_service.get()
+            start_date = request.query_params.get("start_date")
+            end_date = request.query_params.get("end_date")
+
+            if start_date:
+                try:
+                    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+                except ValueError:
+                    raise Exception("Invalid start_date format")
+
+            if end_date:
+                try:
+                    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                except ValueError:
+                    raise Exception("Invalid end_date format")
+
+            response = self.order_warehouse_service.get(start_date=start_date, end_date=end_date)
             return Response(prepare_success_response(response), status=status.HTTP_200_OK)
         except Exception as e:
             return Response(prepare_error_response(str(e)), status=status.HTTP_400_BAD_REQUEST)
