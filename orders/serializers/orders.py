349957d88526b192ba88aede87eac3ecc2c1ba57
Matthew-Rizky-Hartadi <matthew.rizky11@ui.ac.id>
2024-09-26 15:35:38 +1000

    fix: add id field to user details response

--- orders/serializers/orders.py
+++ orders/serializers/orders.py
@@ -26,7 +26,7 @@ class DeliveryDetailsSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'image'] 
+        fields = ['id', 'first_name', 'last_name', 'email', 'image'] 
 
 class OrderSerializer(serializers.ModelSerializer):
 
