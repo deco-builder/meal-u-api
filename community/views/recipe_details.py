309eee40391871ebdf7fffe2eb6b432c97ed5f1c
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-09-28 23:11:41 +1000

    feat: add create recipe

--- community/views/recipe_details.py
+++ community/views/recipe_details.py
@@ -22,3 +22,21 @@ class RecipeDetailsView(APIView):
 
         except Exception as e:
             return Response(prepare_error_response(str(e)), status=status.HTTP_400_BAD_REQUEST)
+
+
+class RecipeView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsWarehouseUser | IsClientUser]
+
+    def __init__(self):
+        self.recipe_details_service = RecipeDetailsService()
+
+    def post(self, request):
+        try:
+            user = request.user
+            image = request.FILES.get("image", None)
+            response = self.recipe_details_service.post(request.data, user, image)
+            return Response(prepare_success_response(response), status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response(prepare_error_response(str(e)), status=status.HTTP_400_BAD_REQUEST)
