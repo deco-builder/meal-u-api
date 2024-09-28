241fc6c61ea546f5c02b21985362592e51f7f05a
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-09-29 00:21:54 +1000

    feat: add meal types GET

--- community/urls.py
+++ community/urls.py
@@ -3,6 +3,7 @@ from .views.recipes import RecipesView
 from .views.recipe_details import RecipeDetailsView, RecipeView
 from .views.mealkits import MealKitsView
 from .views.mealkit_details import MealkitDetailsView
+from .views.meal_type import MealTypeView
 
 urlpatterns = [
     path("recipes/", RecipesView.as_view(), name="Recipes"),
@@ -10,4 +11,5 @@ urlpatterns = [
     path("recipe/<int:recipe_id>/", RecipeDetailsView.as_view(), name="Recipe Details"),
     path("mealkits/", MealKitsView.as_view(), name="Mealkits"),
     path("mealkit/<int:mealkit_id>/", MealkitDetailsView.as_view(), name="Mealkit Details"),
+    path("meal-types/", MealTypeView.as_view(), name="Meal Types"),
 ]
