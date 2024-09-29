9189be517d0072bd4a3a9b98633b5b297b8f2577
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-09-29 17:37:04 +1000

    feat: add role return

--- user_auth/services/google_oauth.py
+++ user_auth/services/google_oauth.py
@@ -30,6 +30,7 @@ class GoogleOAuthService:
                 "email": user.email,
                 "first_name": user.first_name,
                 "last_name": user.last_name,
+                "role": user.role,
                 "profile_picture": image_url,
                 "refresh": str(refresh),
                 "access": str(refresh.access_token),
--- user_auth/services/login.py
+++ user_auth/services/login.py
@@ -19,6 +19,7 @@ class LoginService:
                             "email": user.email,
                             "first_name": user.first_name,
                             "last_name": user.last_name,
+                            "role": user.role,
                             "profile_picture": image_url,
                             "refresh": str(refresh),
                             "access": str(refresh.access_token),
