8e9f53d14a05aea4f7b021843029acf547daabea
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-10-11 20:59:34 +1000

    feat: add category preparation type

--- groceries/models.py
+++ groceries/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     image = models.ImageField(upload_to="category/", blank=True, null=True)
@@ -7,11 +8,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class DietaryDetail(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
 
     def __str__(self):
         return self.name
+
 
 class Unit(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
@@ -19,23 +22,29 @@ class Unit(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     category_id = models.ForeignKey(Category, on_delete=models.PROTECT, null=False, blank=False)
     unit_id = models.ForeignKey(Unit, on_delete=models.PROTECT, null=False, blank=False)
-    unit_size = models.DecimalField(decimal_places=2, max_digits=6, null=False, blank=False, help_text="Size of the product")
+    unit_size = models.DecimalField(
+        decimal_places=2, max_digits=6, null=False, blank=False, help_text="Size of the product"
+    )
     price_per_unit = models.DecimalField(decimal_places=2, max_digits=6, null=False, blank=False)
-    measurement_size = models.DecimalField(decimal_places=2, max_digits=6, null=True, help_text="Size of the measurement unit (e.g., 100 for 100g)")
-    price_per_measurement = models.DecimalField(decimal_places=2, max_digits=6, blank=True, help_text="Price for the given measurement size")
+    measurement_size = models.DecimalField(
+        decimal_places=2, max_digits=6, null=True, help_text="Size of the measurement unit (e.g., 100 for 100g)"
+    )
+    price_per_measurement = models.DecimalField(
+        decimal_places=2, max_digits=6, blank=True, help_text="Price for the given measurement size"
+    )
     description = models.TextField()
     stock = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to="products/", blank=True, null=True)
-    
 
     def calculate_price_per_measurement(self):
         if self.unit_size <= 0 or self.measurement_size <= 0:
             return None
-        
+
         return (self.price_per_unit / self.unit_size) * self.measurement_size
 
     def save(self, *args, **kwargs):
@@ -45,12 +54,23 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class ProductDietaryDetail(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     dietary_details = models.ForeignKey(DietaryDetail, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('product', 'dietary_details')
+        unique_together = ("product", "dietary_details")
+
+
+class CategoryPreparationType(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    additional_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.name + " " + str(self.category)
+
 
 class ProductNutrition(models.Model):
     product_id = models.OneToOneField(Product, primary_key=True, on_delete=models.CASCADE)
@@ -65,7 +85,7 @@ class ProductNutrition(models.Model):
     sugars_per_serving = models.DecimalField(max_digits=6, decimal_places=2, null=True, help_text="in grams")
     dietary_fibre_per_serving = models.DecimalField(max_digits=6, decimal_places=2, null=True, help_text="in grams")
     sodium_per_serving = models.DecimalField(max_digits=6, decimal_places=2, null=True, help_text="in mg")
-    
+
     energy_per_100g = models.DecimalField(max_digits=6, decimal_places=2, null=True, help_text="in kJ")
     protein_per_100g = models.DecimalField(max_digits=6, decimal_places=2, null=True, help_text="in grams")
     fat_total_per_100g = models.DecimalField(max_digits=6, decimal_places=2, null=True, help_text="in grams")
