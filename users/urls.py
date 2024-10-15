7ee5fe9179aba52cf6b95851b2de55f044a9b959
Matthew-Rizky-Hartadi <matthew.rizky11@ui.ac.id>
2024-10-15 19:21:46 +1000

    fix: CreatorDetailView for creator profile

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('payment-methods/', UserPaymentMethodView.as_view(), name='user_payment_methods'),
     # path('user-profile/', UserDetailView.as_view(), name="user-profile"),
     re_path(r'^user-profile/?$', UserDetailView.as_view(), name="user-profile"),
-    path('creator-profile/<int:user_id>/', UserDetailView.as_view(), name="creator-profile")
+    path('creator-profile/<int:user_id>/', CreatorDetailView.as_view(), name="creator-profile")
 ]
